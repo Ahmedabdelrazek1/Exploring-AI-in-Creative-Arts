2e1bb7e0b9fd4b697393a3ef60322566851fdcfe
ahmed abdelrazek <Ahmed.abdelrazek@ejust.edu.eg>
2025-05-09 03:21:01 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Exploring-AI-in-Creative-Arts/', // ✅ ADD THIS LINE
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
